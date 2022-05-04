8ccbd1222f855d377f0b56ad9b12eb3afa6c007f
Brooke Love <savienloveb@gmail.com>
2022-05-04 11:01:29 -0700

    updating location model

---
+++ models/Location.js
@@ -0,0 +1,27 @@
+const {MOdel, DataTypes} = require('sequelize');
+const sequelize = require ('../config/connection');
+
+class Location extends Model {}
+
+Location.init ({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    location_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'car',
+  }
+)
+
+module.exports = Location;
